75203240dfdb18efbf2771c06f4015c22457af22
caoxudong <caoxudong818@gmail.com>
2026-08-03 22:45:37 +0800

    add iterator and json functions for concurrent component

--- component/concurrent_component.go
+++ component/concurrent_component.go
@@ -1,6 +1,9 @@
 package component
 
-import "sync"
+import (
+	"encoding/json"
+	"sync"
+)
 
 type SyncMap[K comparable, V any] struct {
 	m sync.Map
@@ -30,4 +33,30 @@ func (s *SyncMap[K, V]) Keys() []K {
 		return true
 	})
 	return keys
+}
+
+func (s *SyncMap[K, V]) Range(f func(key K, value V) bool) {
+	s.m.Range(func(key, value any) bool {
+		return f(key.(K), value.(V))
+	})
+}
+
+func (s *SyncMap[K, V]) MarshalJSON() ([]byte, error) {
+	tmpMap := make(map[K]V)
+	s.Range(func(key K, value V) bool {
+		tmpMap[key] = value
+		return true
+	})
+	return json.Marshal(tmpMap)
+}
+
+func (s *SyncMap[K, V]) UnmarshalJSON(data []byte) error {
+	tmpMap := make(map[K]V)
+	if err := json.Unmarshal(data, &tmpMap); err != nil {
+		return err
+	}
+	for key, value := range tmpMap {
+		s.Store(key, value)
+	}
+	return nil
 }
